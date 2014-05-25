6b2ceb6f3369fed3ae1113c38c648e7fa8f87054
Mitch Souders <mitch.souders@gmail.com>
2014-05-25 15:22:53 -0700

    Merged master into llvm_codegen

--- src/runtime_x86.c
+++ src/runtime_x86.c
@@ -5,7 +5,7 @@ extern void Main_main();
 extern void MJCStatic_init();
 
 void System_out(int x) {
-    printf("%d\n",x);
+  printf("%d\n", x);
 }
 
 #define HEAPLEN (4 * 1024 * 1024)
@@ -18,35 +18,35 @@ void MJC_globalRoot(void *root) {
 }
 
 int* MJC_allocObject(int size) {
-    int* newObj = heap+freeHeap;
-    if (size+freeHeap >= HEAPLEN) {
-        fprintf(stderr,"Out of memory!");
-        exit(1);
-    }
-    freeHeap += size;
-    return newObj;
+  int* newObj = heap + freeHeap;
+  if (size + freeHeap >= HEAPLEN) {
+    fprintf(stderr, "Out of memory!");
+    exit(1);
+  }
+  freeHeap += size;
+  return newObj;
 }
 
 int* MJC_allocArray(int elementSize, int len) {
-    int size = elementSize * len;
-    int* newArr = heap+freeHeap;
-    if (size+freeHeap >= HEAPLEN) {
-        fprintf(stderr,"Out of memory!");
-        exit(1);
-    }
-    freeHeap += size;
-    return newArr;
+  int size = elementSize * len;
+  int* newArr = heap + freeHeap;
+  if (size + freeHeap >= HEAPLEN) {
+    fprintf(stderr, "Out of memory!");
+    exit(1);
+  }
+  freeHeap += size;
+  return newArr;
 
 }
 
 void MJC_putc(char c) {
-    printf("%c", c);
+  printf("%c", c);
 }
 int main() {
-    //    printf("Starting:\n");
-    MJCStatic_init();
-    Main_main();
-    //    printf("Finishing (%d words allocated).\n",freeHeap);
-    return 0;
+  //    printf("Starting:\n");
+  MJCStatic_init();
+  Main_main();
+  //    printf("Finishing (%d words allocated).\n",freeHeap);
+  return 0;
 }
 
